7081b4c62d0c62db492496c34a9231e849aa47f8
Kpennie <kpennie0@gmail.com>
2025-07-28 15:28:04 +0000

    Traffic Light

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,54 @@
-import React from "react";
+// PURPOSE: Reusable traffic light component
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from 'react';
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
+const TrafficLight = () => {
+    const [color, setColor] = useState("red");
+
+    const handleLightClick = (selectedColor) => {
+        setColor(selectedColor);
+    };
+    
+
+    const cycleColors = () => {
+        if (color === "red") {
+            setColor("yellow");
+        } else if (color === "yellow") {
+            setColor("green");
+        } else {
+            setColor("red");
+        }
+    };
+
+    return (
+        <div className="traffic-light-container">
+            <div className="traffic-top"></div>
+            <div className="traffic-light">
             
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+                <div 
+                    className={`light red ${color === "red" ? "selected" : ""}`}
+                    onClick={() => handleLightClick("red")}
+                ></div>
+                
+                <div 
+                    className={`light yellow ${color === "yellow" ? "selected" : ""}`}
+                    onClick={() => handleLightClick("yellow")}
+                ></div>
+    
+                <div 
+                    className={`light green ${color === "green" ? "selected" : ""}`}
+                    onClick={() => handleLightClick("green")}
+                ></div>
+            </div>
+            
+            <button 
+                className="cycle-button"
+                onClick={cycleColors}
+            >
+                Next Color
+            </button>
+        </div>
+    );
 };
 
-export default Home;
+export default TrafficLight;
